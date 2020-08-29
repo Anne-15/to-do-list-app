68f15cfb4f60837b533dec5a0cbafeb29de3acd0
Anne-15 <wariiyuanne@gmail.com>
2020-08-29 13:52:19 +0300

    creating first app

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# Create a representaion of the database tables
+class Todo(models.Model):
+    added_date = models.DateTimeField()
+    todo_item = models.CharField(max_length=200)
