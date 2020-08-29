0e8b7d4732c4dd9f7e2b194052f14bc32ccd78d2
Anne-15 <wariiyuanne@gmail.com>
2020-08-29 17:12:28 +0300

    adding items to the to do list

--- todoApp/models.py
+++ todoApp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 # Create a representaion of the database tables
-class Todo(models.Model):
+class TodoList(models.Model):
     added_date = models.DateTimeField()
-    todo_item = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
--- todoApp/views.py
+++ todoApp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from todoApp.models import TodoList
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'todoApp/index.html')
+    todo_items = TodoList.objects.all().order_by("added_date")
+
+    return render(request, 'todoApp/index.html',{
+        "todo_items": todo_items
+    })
+
+@csrf_exempt
+def add_item(request):
+    current_date = timezone.now()
+    content_items = request.POST["content"]
+    TodoList.objects.create(added_date = current_date, text = content_items)
+    return HttpResponseRedirect('/')
